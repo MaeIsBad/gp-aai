104eae206713130aa76c374e884bfc65339c536c
Stephan Stanisic <stephan.stanisic@windesheim.nl>
2022-03-11 14:05:00 +0100

    cleanup build

--- build/CMakeProject1/CMakeFiles/CMakeDemo.dir/gl2/Context.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeProject1/CMakeFiles/CMakeDemo.dir/gl2/Context.cpp.o: \
- /home/steph/Projects/Windowsheim/gp-aai/CMakeProject1/gl2/Context.cpp \
- /nix/store/5dnhkhs2a3fb323d6xcx9pvbwvrhxh25-glibc-2.33-108-dev/include/stdc-predef.h
